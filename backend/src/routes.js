51f09ba745b93c9229f4eda3ee25e95f9bb77624
Romulo <romulosousa27@hotmail.com>
2019-08-08 18:04:57 -0300

    Refatorando as rotas, inserindo os metodos de Like e Cadastro de user.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,10 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  // return res.json('Hello NodeJS');
-  return res.json({
-    message: `Olá, ${ req.query.name }`
-  });
-});
-
-routes.post('/devs', (req, res) => {
-  return res.json(req.body);
-});
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
