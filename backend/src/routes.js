131dc2591c7ccbc22268bb70a5124bd8cb0326ec
Romulo <romulosousa27@hotmail.com>
2019-08-08 14:15:58 -0300

    Rotas da api

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  // return res.json('Hello NodeJS');
+  return res.json({
+    message: `Olá, ${ req.query.name }`
+  });
+});
+
+routes.post('/devs', (req, res) => {
+  return res.json(req.body);
+});
+
+module.exports = routes;
