3448f1736c823842bbbd05273e702d8ed62f7dc0
Romulo <romulosousa27@hotmail.com>
2019-08-09 11:27:10 -0300

    rotas da aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+// Test
+routes.get('/test', (req, res) => {
+  return res.json({message: 'Hello World'})
+});
+
+// Devs
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+// Likes and Dislikes
 routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
