33d10c0a926a536c16b3e27a10ef36e80982e9c5
Romulo <romulosousa27@hotmail.com>
2019-08-09 11:25:27 -0300

    Controller de Dislike

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,22 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res){
+    const {user} = req.headers;
+    const {devId} = req.param;
+
+    // user logged
+    const loggedDev = await Dev.findById(user);
+    // user receive the liked
+    const targetDev = await Dev.findById(devId);
+
+    if(!targetDev){
+      return res.status(400).json({ error: 'Dev not exists' });
+    }
+
+    loggedDev.dislikes.push(targetDev._id);
+    await loggedDev.save();
+
+    return res.json(loggedDev)
+  }
+};
