4e213c5dfa0a869981d445c20ebdb650c88e1ff0
Romulo <romulosousa27@hotmail.com>
2019-08-09 18:06:06 -0300

    inserindo o metodo de buscar o usuário do github

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './../services/api'
+
 import './Login.css';
 
 import logo from './../assets/logo.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+    const [ username, setUsername ] = useState('');
+
+    async function handleSubmit(e) { 
+        e.preventDefault();
+        
+        const response = await api.post('/devs', { username });
+        
+        const { _id } = response.data;
+        console.log(response);        
+        history.push(`/dev/${_id}`);
+
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="TinDev" />
-                <input placeholder="Digite seu usuário no Github" />
+                <input placeholder="Digite seu usuário no Github" value={username} onChange={e => setUsername(e.target.value)} />
                 <button type="submit">Enviar</button>
             </form>
         </div>
     );
-}
+} 
