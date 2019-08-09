7237d6d68b86a4f15657c664844b1f149e63bc4a
Romulo <romulosousa27@hotmail.com>
2019-08-09 11:27:46 -0300

    modificando o servido, inserindo o cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const server = express();
 
+// MongoDB
 mongoose.connect('mongodb+srv://romulo:romulo@cluster-test-agcll.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3000);
+server.listen(3333);
